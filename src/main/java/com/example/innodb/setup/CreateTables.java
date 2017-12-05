a9273c69150f0056df99eeaa1f8064571ed3312a
Tom Denley <tom.denley@timgroup.com>
2017-12-05 15:28:30 +0000

    tomd: example table with more rows

--- src/main/java/com/example/innodb/setup/CreateTables.java
+++ src/main/java/com/example/innodb/setup/CreateTables.java
@@ -3,6 +3,7 @@ package com.example.innodb.setup;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Random;
 
 public final class CreateTables {
 
@@ -14,27 +15,30 @@ public final class CreateTables {
 
         try (Connection connection = DriverManager.getConnection(url)) {
             createTable(connection, "t1");
-            generateRows(connection,"t1");
+            generateRows(connection,"t1", 3);
 
             createTable(connection, "t2");
+            generateRows(connection,"t2", 1000);
+
+            createTable(connection, "t3");
+            generateRows(connection,"t3", 100000);
         }
 
     }
 
-    private static void generateRows(Connection connection, String tableName) throws Exception {
+    private static void generateRows(Connection connection, String tableName, int count) throws Exception {
+        Random random = new Random();
         try (Statement statement = connection.createStatement()) {
-            statement.execute("insert into " + tableName +
-                    "(pk, v1)" +
-                    " VALUES " +
-                    "(1, 9472), " +
-                    "(2, 452), " +
-                    "(3, 69463)"
-            );
+            for(int i = 0; i < count; i++) {
+                random.nextInt();
+                statement.execute("insert into " + tableName + "(pk, v1) VALUES (" + i + ", " + random.nextInt() + ")");
+            }
         }
     }
 
     private static void createTable(Connection connection, String tableName) throws Exception {
         try (Statement statement = connection.createStatement()) {
+            statement.execute("drop table if exists " + tableName);
             statement.execute("create table if not exists " + tableName + "(" +
                     "pk int primary key, " +
                     "v1 int not null " +
